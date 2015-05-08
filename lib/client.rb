5185a26b6b98ab375852500eb507c0a8186f8f25
Frank <jedigene@gmail.com>
2015-05-08 10:18:01 -0700

    spec for client.== fails as expected.

--- lib/client.rb
+++ lib/client.rb
@@ -9,16 +9,23 @@ class Client
   define_singleton_method(:all) do
     returned_clients = DB.exec("SELECT * FROM clients;")
     clients = []
-    returned_clients.each() do |client|
+    returned_clients.each do |client|
       name = client.fetch("name")
-      id = patron.fetch("id").to_i()
+      id = patron.fetch("id").to_i
       clients.push(Client.new({:name => name, :id => nil}))
     end
     clients
   end
 
+  define_singleton_method(:find) do |id|
+    @id = id
+    result = DB.exec("SELECT * FROM clients WHERE id = #{@id}")
+    @name = result.first.fetch("name")
+    Client.new({:name => @name, :id => @id})
+  end
+
   define_method(:save) do
     result = DB.exec("INSERT INTO clients (name) VALUES ('#{@name}') RETURNING id;")
-    @id = result.first().fetch("id").to_i()
+    @id = result.first.fetch("id").to_i
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -5,7 +5,7 @@ describe(Client) do
   describe('#name') do
     it("returns the client's name") do
       client = Client.new({:name => "Gene", :id => nil})
-      expect(client.name()).to(eq("Gene"))
+      expect(client.name).to(eq("Gene"))
     end
   end
 
@@ -13,23 +13,31 @@ describe(Client) do
     it("sets the id when you save it") do
       client = Client.new({:name => "Gene", :id => nil})
       client.save
-      expect(client.id()).to(be_an_instance_of(Fixnum))
+      expect(client.id).to(be_an_instance_of(Fixnum))
     end
   end
 
   describe(".all") do
     it("starts off with no clients") do
-      expect(Client.all()).to(eq([]))
+      expect(Client.all).to(eq([]))
     end
   end
-end
 
   describe(".find") do
     it("returns a client by its ID number") do
       test_client = Client.new({:name => "Robert", :id => nil})
-      test_client.save()
+      test_client.save
       test_client2 = Client.new({:name => "Sam", :id => nil})
-      test_client2.save()
-      expect(Client.find(test_client2.id())).to(eq(test_client2))
+      test_client2.save
+      expect(Client.find(test_client2.id)).to(eq(test_client2))
     end
+  end
+
+  describe("#==") do
+    it("is the same client if it has the same name and id") do
+      client = Client.new({:name => "Gene", :id => nil})
+      client2 = Client.new({:name => "Gene", :id => nil})
+      expect(client).to(eq(client2))
+    end
+  end
 end
