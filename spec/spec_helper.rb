89e57ae59e2d9ea4b45bacae48bc094ceea36bca
Frank <jedigene@gmail.com>
2015-05-08 09:27:39 -0700

    spec for client.name now passes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ DB = PG.connect({:dbname => 'hair_salon'})
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec("DELETE FROM stylist *;")
-    DB.exec("DELETE FROM client *;")
+    DB.exec("DELETE FROM stylists *;")
+    DB.exec("DELETE FROM clients *;")
   end
 end
